e975f60fc4723af1af8ea92bcecd92b5045216ec
Timmy_MT <mathcore_detected@mail.ru>
2019-08-31 19:40:50 +0400

    refactoring action show into q_controller_spec

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,8 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -14,6 +14,21 @@ RSpec.describe QuestionsController, type: :controller do
     it 'renders index view' do
       expect(response).to render_template(:index)
     end
+  end
+
+  describe "GET #show" do
+    let(:question) { create(:question) }
+
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template(:show)
+    end
 
   end
+
 end
