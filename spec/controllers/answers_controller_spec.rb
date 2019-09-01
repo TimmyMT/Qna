da702bbda45f8977f92c7c4ad61476d71a2188c4
Timmy_MT <mathcore_detected@mail.ru>
2019-09-01 19:01:06 +0400

    Rspec CRUD tests for answers controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -34,13 +34,43 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'PATH #update' do
     let(:question) { create(:question) }
+    let(:answer) { create(:answer, question: question) }
 
     context 'valid' do
+      it 'updated answer' do
+        patch :update, params: {id: answer, question_id: question, answer: attributes_for(:answer)}
+        expect(assigns(:answer)).to eq answer
+      end
 
+      it 'changes answer attributes' do
+        patch :update, params: {id: answer, question_id: question, answer: {body: 'new'}}
+        answer.reload
+
+        expect(answer.body).to eq 'new'
+      end
     end
 
     context 'invalid' do
+      it 'not updated answer' do
+        patch :update, params: {id: answer, question_id: question, answer: attributes_for(:answer, :invalid_answer)}
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
 
+  describe 'DELETE #destroy' do
+    let(:question) { create(:question) }
+    let!(:answer) { create(:answer, question: question) }
+
+    it 'deleted answer' do
+      expect do
+        delete :destroy, params: {id: answer}
+      end.to change(Answer, :count).by(-1)
+    end
+
+    it 'redirect to question page after deleted' do
+      delete :destroy, params: {id: answer}
+      expect(response).to redirect_to(question)
     end
   end
 
