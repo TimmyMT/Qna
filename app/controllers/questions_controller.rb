10dd72ea24f83a07e03ccb44c52ecea8ce2390c2
Timmy_MT <mathcore_detected@mail.ru>
2019-09-01 14:02:31 +0400

    remove before_action from questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,4 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -10,7 +9,6 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
   end
 
   def create
@@ -27,7 +25,7 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
+    if question.update(question_params)
       redirect_to @question
     else
       render :edit
@@ -35,15 +33,17 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question.destroy
+    question.destroy
     redirect_to questions_path
   end
 
   private
 
-  def set_question
-    @question = Question.find(params[:id])
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
   end
+
+  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -20,10 +20,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe "GET #show" do
     before { get :show, params: { id: question } }
 
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
-
     it 'renders show view' do
       expect(response).to render_template(:show)
     end
@@ -32,10 +28,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe "GET #new" do
     before { get :new }
 
-    it 'assigns a new Question to @question' do
-      expect(assigns(:question)).to be_a_new(Question)
-    end
-
     it 'renders new view' do
       expect(response).to render_template(:new)
     end
@@ -43,10 +35,6 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #edit' do
     before { get :edit, params: { id: question } }
-
-    it 'assigns edit @question' do
-      expect(assigns(:question)).to eq question
-    end
 
     it 'renders edit view' do
       expect(response).to render_template(:edit)
