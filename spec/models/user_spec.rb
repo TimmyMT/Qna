7f987786362cbb6738310f3c4b45dcaacaee60ed
Timmy_MT <mathcore_detected@mail.ru>
2019-09-05 18:57:20 +0400

    add unit spec models, for check 'is_creator?' instance method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe User, type: :model do
   it { should have_many(:answers) }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'Check instance method for author of object' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:wrong_user) { create(:user) }
+
+    it 'return true for author input:user' do
+      expect(user.is_creator?(question.user)).to eq(true)
+    end
+
+    it 'return false for not author input:wrong_user' do
+      expect(wrong_user.is_creator?(question.user)).to eq(false)
+    end
+  end
 end
