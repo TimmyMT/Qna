0169b883fcec1420fe8647b12fa2c84c760d1d1c
Timmy_MT <mathcore_detected@mail.ru>
2019-09-05 13:38:10 +0400

    add author method into model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
 
-  def have_permission?(object)
-    if current_user.present?
-      object.user == current_user
-    else
-      false
-    end
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   has_many :questions, dependent: :nullify
   has_many :answers, dependent: :nullify
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def is_creator?(object)
+    self == object.user
+  end
+
 end
