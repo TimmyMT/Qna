7e23ffe093ab4259db41c1c83fafa33e44912192
Timmy_MT <mathcore_detected@mail.ru>
2019-09-08 21:44:14 +0400

    refactoring routes for select_best_answer

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'questions#index'
-  patch 'questions/:id/select_best_answer/:answer_id', to: 'questions#select_best_answer', as: 'select_best_answer'
+
+
 
   resources :questions, shallow: true do
+    member do
+      patch 'select_best_answer/:answer_id', to: 'questions#select_best_answer', as: 'select_best_answer'
+    end
     resources :answers, shallow: true
   end
 end
