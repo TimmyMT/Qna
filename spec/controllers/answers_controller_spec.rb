f2fca0b5d034965241f325693cc7cf7a06234800
Timmy_MT <mathcore_detected@mail.ru>
2019-09-08 19:49:03 +0400

    hide best answer from default answers collection

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,35 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
-  describe 'GET #edit' do
-    let(:user) { create(:user) }
-    let(:question) { create(:question, user: user) }
-    let(:answer) { create(:answer, question: question, user: user) }
-
-    context 'Authorized user' do
-      # it 'render edit view for author' do
-      #   login(user)
-      #   get :edit, params: { id: answer, question: question, user: user }
-      #   expect(response).to render_template(:edit)
-      # end
-
-      # it 'redirect to question view for not author' do
-      #   user = FactoryBot.create(:user)
-      #   login(user)
-      #   get :edit, params: { id: answer, question: question, user: user }
-      #
-      #   expect(response).to redirect_to(question)
-      # end
-    end
-
-    context 'Not Authorized user' do
-      # it 'redirect to sign in' do
-      #   get :edit, params: { id: answer }
-      #   expect(response).to redirect_to new_user_session_path
-      # end
-    end
-  end
-
   describe 'POST #create' do
     let(:user) { create(:user) }
     let(:question) { create(:question, user: user) }
@@ -96,12 +67,6 @@ RSpec.describe AnswersController, type: :controller do
       before { login(user) }
 
       context 'valid' do
-        # it 'updated answer' do
-        #   patch :update, params: {id: answer, question_id: question, answer: attributes_for(:answer), user: user, format: :js}
-        #
-        #   expect(assigns(:answer)).to eq answer
-        # end
-
         it 'changes answer attributes' do
           patch :update, params: {id: answer, question_id: question, answer: {body: 'new'}, user: user, format: :js}
           answer.reload
@@ -146,35 +111,16 @@ RSpec.describe AnswersController, type: :controller do
     let(:user) { create(:user) }
     let(:question) { create(:question, user: user) }
     let!(:answer) { create(:answer, question: question, user: user) }
-  #
-  #   context 'Guest' do
-  #     it 'tries to delete answer' do
-  #       expect { delete :destroy, params: {id: answer} }.to_not change(Answer, :count)
-  #     end
-  #   end
-  #
+
     context 'Authorized user' do
       before { login(user) }
-  #
+
       context 'User is answer author' do
         it 'deleted answer' do
           expect(answer.user).to eq(user)
           expect { delete :destroy, params: {id: answer, user: user}, format: :js }.to change(Answer, :count).by(-1)
         end
-  #
-  #       it 'redirect to question page after deleted' do
-  #         delete :destroy, params: {id: answer, user: user}
-  #         expect(response).to redirect_to(question)
-  #       end
       end
-  #
-  #     context 'User is not answer author' do
-  #       it 'deleted answer' do
-  #         user = FactoryBot.create(:user)
-  #         login(user)
-  #         expect { delete :destroy, params: {id: answer, user: user} }.to_not change(Answer, :count)
-  #       end
-  #     end
     end
 
   end
