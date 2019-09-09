38dd920e176e7d7f2000dd3dd555846f75c7b8b5
Timmy_MT <mathcore_detected@mail.ru>
2019-09-09 21:59:38 +0400

    green acceptance for 'select best answer'

--- spec/features/questions/choose_best_answer_of_question_spec.rb
+++ spec/features/questions/choose_best_answer_of_question_spec.rb
@@ -5,9 +5,10 @@ feature 'User can choose best answer', %q{
 } do
 
   given(:user) { create(:user) }
+  given(:wrong_user) { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:first_answer) { create(:answer, question: question, user: user) }
-  given!(:two_answer) { create(:answer, question: question, user: user) }
+  given!(:second_answer) { create(:answer, question: question, user: user) }
 
   describe 'Authorized user' do
     background do
@@ -19,25 +20,43 @@ feature 'User can choose best answer', %q{
     scenario 'choose best answer', js: true do
       within ".answer_#{first_answer.id}" do
         click_on 'Make it best'
+        expect(page).to have_css '.best_answer_of_question'
+        expect(page).to have_content 'Best answer!'
       end
 
-      within ".best_answer_of_question" do
-        expect(page).to have_content first_answer.body
-      end
-
-      within ".answers" do
-        expect(page).to_not have_css ".answer_#{first_answer.id}"
-        expect(page).to have_css ".answer_#{two_answer.id}"
-      end
-
-      within ".answer_#{two_answer.id}" do
+      within ".answer_#{second_answer.id}" do
+        expect(page).to_not have_content 'Best answer!'
         click_on 'Make it best'
+        expect(page).to have_css '.best_answer_of_question'
+        expect(page).to have_content 'Best answer!'
       end
 
-      within ".answers" do
-        expect(page).to have_css ".answer_#{first_answer.id}"
-        expect(page).to_not have_css ".answer_#{two_answer.id}"
+      within ".answer_#{first_answer.id}" do
+        expect(page).to_not have_css '.best_answer_of_question'
+        expect(page).to_not have_content 'Best answer!'
       end
+    end
+  end
+
+  describe 'Wrong user' do
+    background do
+      sign_in(wrong_user)
+
+      visit question_path(question)
+    end
+
+    scenario 'tries choose best answer' do
+      expect(page).to_not have_link 'Make it best'
+    end
+  end
+
+  describe 'Guest' do
+    background do
+      visit question_path(question)
+    end
+
+    scenario 'tries choose best answer' do
+      expect(page).to_not have_link 'Make it best'
     end
   end
 
