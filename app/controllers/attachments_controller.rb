8c525c88e9657a66249a1292e75341b74ef05732
Timmy_MT <mathcore_detected@mail.ru>
2019-09-12 04:46:41 +0400

    author of object can delete file

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,10 +1,16 @@
 class AttachmentsController < ApplicationController
 
   def delete_attachment
-    @file = ActiveStorage::Attachment.find(params[:id])
+    file = ActiveStorage::Attachment.find(params[:id])
+    class_name = file.record_type.constantize
 
-    @file.purge
-    redirect_to question_path(@file.record_id)
+    if current_user&.creator?(class_name.find(file.record_id))
+      file.purge
+
+      redirect_to request.referer, notice: 'File successfully deleted'
+    else
+      redirect_to request.referer, alert: 'File not deleted'
+    end
   end
 
 end
