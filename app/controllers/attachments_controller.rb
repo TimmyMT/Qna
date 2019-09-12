52321f8a7ac8538169eb3be2e3e5a0e351bd4607
Timmy_MT <mathcore_detected@mail.ru>
2019-09-12 16:09:19 +0400

    deleting files with ajax

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,16 +1,10 @@
 class AttachmentsController < ApplicationController
 
   def delete_attachment
-    file = ActiveStorage::Attachment.find(params[:id])
-    class_name = file.record_type.constantize
+    @file = ActiveStorage::Attachment.find(params[:id])
+    class_name = @file.record_type.constantize
 
-    if current_user&.creator?(class_name.find(file.record_id))
-      file.purge
-
-      redirect_to request.referer, notice: 'File successfully deleted'
-    else
-      redirect_to request.referer, alert: 'File not deleted'
-    end
+    @file.purge if current_user&.creator?(class_name.find(@file.record_id))
   end
 
 end
--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,10 @@
 module AttachmentsHelper
+  def record_creator?(file)
+    class_name = file.record_type.constantize
+    return current_user&.creator?(class_name.find(file.record_id))
+  end
+
+  def files_present?(object)
+    object.files.attached?
+  end
 end
