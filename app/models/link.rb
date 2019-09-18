93dba31fb0db31153233248bd15cd6b6aeeaa23b
Timmy_MT <mathcore_detected@mail.ru>
2019-09-18 14:19:04 +0500

    add gist view into answer link

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,17 @@ class Link < ApplicationRecord
 
   validates :name, :url, presence: true
   validates :url, format: { with: URI.regexp }
+
+  def gist?
+    host = URI.parse(url).host
+    return true if host&.include?('gist.github.com')
+    false
+  end
+
+  def gist_id
+    if gist?
+      url.split('/').reject!(&:empty?).last
+    end
+  end
+
 end
