826ff73e2a333561fe9883e7850d76b3ba236a0c
Timmy_MT <mathcore_detected@mail.ru>
2019-09-18 13:21:14 +0500

    fix add links for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -37,7 +37,7 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    params.require(:answer).permit(:body, files: [], links_attributes: [:name, :url])
+    params.require(:answer).permit(:body, files: [], links_attributes: [:name, :url, :done, :_destroy])
   end
 
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,10 @@
 class Answer < ApplicationRecord
-  has_many :links, dependent: :destroy, as: :linkable
+  has_many :links, dependent: :destroy, as: :linkable, inverse_of: :linkable
   belongs_to :question
   belongs_to :user
   has_many_attached :files, dependent: :destroy
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank
+  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   validates :body, presence: true
 
