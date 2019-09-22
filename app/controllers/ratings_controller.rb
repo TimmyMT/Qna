02ccf0cebd2bf5b8640c260bb5d9afbcccdbb5eb
Timmy_MT <mathcore_detected@mail.ru>
2019-09-22 17:22:16 +0500

    author of object can't vote for his object

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,58 +5,33 @@ class RatingsController < ApplicationController
   def up
     @rating.up(current_user)
 
-    respond_to do |format|
-      if @rating.save
-        format.json { render json: @rating }
-      end
-    end
-
-    # @rating.save
-    # redirect_after_action
+    render_rating_json
   end
 
   def down
     @rating.down(current_user)
 
-    respond_to do |format|
-      if @rating.save
-        format.json { render json: @rating }
-      end
-    end
-
-    # @rating.save
-    # redirect_after_action
+    render_rating_json
   end
 
   def clear_vote_from
     @rating.clear_vote(current_user)
 
+    render_rating_json
+  end
+
+  private
+
+  def render_rating_json
     respond_to do |format|
       if @rating.save
         format.json { render json: @rating }
       end
     end
-
-    # @rating.save
-    # redirect_after_action
   end
-
-  private
 
   def find_rating
     @rating = Rating.find(params[:id])
-  end
-
-  def redirect_after_action
-    @klass = @rating.ratingable_type.constantize
-
-    if @klass == Answer
-      @question = @klass.find(@rating.ratingable_id).question
-    else
-      @question = @klass.find(@rating.ratingable_id)
-    end
-
-    redirect_to question_path(@question)
   end
 
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,7 +4,7 @@ class Rating < ApplicationRecord
   serialize :votes, Hash
 
   def up(user)
-    unless self.votes.key?(user.id)
+    unless self.votes.key?(user.id) && object_creator(user)
       transaction do
         self.votes[user.id] = '+'
         self.value += 1
@@ -13,7 +13,7 @@ class Rating < ApplicationRecord
   end
 
   def down(user)
-    unless self.votes.key?(user.id)
+    unless self.votes.key?(user.id) && object_creator(user)
       transaction do
         self.votes[user.id] = '-'
         self.value -= 1
@@ -22,12 +22,19 @@ class Rating < ApplicationRecord
   end
 
   def clear_vote(user)
-    if self.votes.key?(user.id)
+    if self.votes.key?(user.id) && !object_creator(user)
       transaction do
         self.value -= 1 if self.votes[user.id] == '+'
         self.value += 1 if self.votes[user.id] == '-'
         self.votes.delete(user.id)
       end
     end
+  end
+
+  private
+
+  def object_creator(user)
+    @object = self.ratingable_type.constantize.find(self.ratingable_id)
+    user.creator?(@object)
   end
 end
