5c39bd317008db12caab9b872faf39669fcb2c83
Timmy_MT <mathcore_detected@mail.ru>
2019-10-04 15:14:31 +0500

    delete var user_signed_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,6 @@ class ApplicationController < ActionController::Base
   def gon_user
     if signed_in?
       gon.current_user_id = current_user.id
-      gon.user_signed_in = true
-    else
-      gon.user_signed_in = false
     end
   end
 
