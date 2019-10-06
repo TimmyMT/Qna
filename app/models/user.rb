95f12b544faad7510243b363ddc961455b701033
Timmy_MT <mathcore_detected@mail.ru>
2019-10-06 22:13:22 +0500

    refactoring find_for_auth service

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
   end
 
   def create_authorization(auth)
-    self.authorizations.create(provider: auth.provider, uid: auth.uid)
+    self.authorizations.create!(provider: auth.provider, uid: auth.uid)
   end
 
 end
--- app/services/find_for_oauth.rb
+++ app/services/find_for_oauth.rb
@@ -6,18 +6,17 @@ class Services::FindForOauth
   end
 
   def call
-    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
     return authorization.user if authorization
 
     email = auth.info[:email]
-    user = User.where(email: email).first
-    if user
-      user.create_authorization(auth)
-    else
+    user = User.find_by(email: email)
+
+    unless user
       password = Devise.friendly_token[0, 20]
       user = User.create!(email: email, password: password, password_confirmation: password)
-      user.create_authorization(auth)
     end
+    user.create_authorization(auth)
 
     user
   end
