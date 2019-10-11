046e1b3dc4fb04fa283594692b39bd6680306e0b
Timmy_MT <mathcore_detected@mail.ru>
2019-10-11 13:35:58 +0500

    profiles spec users list

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -34,4 +34,38 @@ describe 'Profiles API', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/profiles' do
+    let!(:me) { create(:user) }
+    let!(:users) { create_list(:user, 2) }
+    let!(:method) { :get }
+    let!(:api_path) { '/api/v1/profiles' }
+    it_behaves_like 'API Authorizable'
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before do
+        get api_path, params: { access_token: access_token.token }, headers: headers
+      end
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'return all users list without current_user' do
+        expect(json.size).to eq 2
+
+        json.each do |object|
+          expect(object['email']).to_not eq me.email
+        end
+      end
+
+      it 'returns all public fields' do
+        %w[id email created_at updated_at].each do |attr|
+          expect(json.first[attr]).to eq users.first.send(attr).as_json
+        end
+      end
+    end
+  end
 end
