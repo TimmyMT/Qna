5eb1d5e425275075007905b1ce5aad45bcf1e1cd
Timmy_MT <mathcore_detected@mail.ru>
2019-10-15 00:00:18 +0500

    subscription unit spec

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,5 +3,13 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:question) }
-  it { should validate_uniqueness_of(:user_id).scoped_to(:question_id) }
+
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
+
+  it 'uniqueness validate' do
+    Subscription.create(user: user, question: question)
+    @double_sub = Subscription.create(user: user, question: question)
+    @double_sub.errors.full_messages.should eq ["User has already been taken"]
+  end
 end
