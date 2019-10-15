cc21f0e7dafc666294e3f3f4d29eded6be585e99
Timmy_MT <mathcore_detected@mail.ru>
2019-10-15 13:06:35 +0500

    fixed daily mailer specs

--- spec/mailers/daily_mailer_spec.rb
+++ spec/mailers/daily_mailer_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe DailyMailer, type: :mailer do
   describe "digest" do
     let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+    let!(:question) { create(:question, user: other_user) }
     let(:mail) { DailyMailer.digest(user) }
 
     it 'renders the headers' do
@@ -11,6 +13,10 @@ RSpec.describe DailyMailer, type: :mailer do
 
     it 'renders the body' do
       expect(mail.body.encoded).to match("Hello")
+
+      Question.where('created_at > ?', 1.day.ago).each do |question|
+        expect(mail.body.encoded).to match(question.title)
+      end
     end
   end
 end
