0cd1b6c9dfc39fced5bc278ed60916e861f4fa24
Timmy_MT <mathcore_detected@mail.ru>
2019-10-19 17:05:04 +0500

    add schema migration

---
+++ db/migrate/20191019120304_load_db_from_schema.rb
@@ -0,0 +1,182 @@
+class LoadDbFromSchema < ActiveRecord::Migration[5.2]
+  def change
+    create_table "achievements", force: :cascade do |t|
+      t.string "name"
+      t.bigint "question_id"
+      t.bigint "user_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["question_id"], name: "index_achievements_on_question_id"
+      t.index ["user_id"], name: "index_achievements_on_user_id"
+    end
+
+    create_table "active_storage_attachments", force: :cascade do |t|
+      t.string "name", null: false
+      t.string "record_type", null: false
+      t.bigint "record_id", null: false
+      t.bigint "blob_id", null: false
+      t.datetime "created_at", null: false
+      t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+      t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    end
+
+    create_table "active_storage_blobs", force: :cascade do |t|
+      t.string "key", null: false
+      t.string "filename", null: false
+      t.string "content_type"
+      t.text "metadata"
+      t.bigint "byte_size", null: false
+      t.string "checksum", null: false
+      t.datetime "created_at", null: false
+      t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+    end
+
+    create_table "answers", force: :cascade do |t|
+      t.text "body"
+      t.bigint "question_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.bigint "user_id"
+      t.boolean "best", default: false
+      t.index ["question_id"], name: "index_answers_on_question_id"
+      t.index ["user_id"], name: "index_answers_on_user_id"
+    end
+
+    create_table "authorizations", force: :cascade do |t|
+      t.bigint "user_id"
+      t.string "provider"
+      t.string "uid"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["provider", "uid"], name: "index_authorizations_on_provider_and_uid"
+      t.index ["user_id"], name: "index_authorizations_on_user_id"
+    end
+
+    create_table "comments", force: :cascade do |t|
+      t.text "body", null: false
+      t.string "commentable_type"
+      t.bigint "commentable_id"
+      t.bigint "user_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+      t.index ["user_id"], name: "index_comments_on_user_id"
+    end
+
+    create_table "links", force: :cascade do |t|
+      t.string "name"
+      t.string "url"
+      t.string "linkable_type"
+      t.bigint "linkable_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["linkable_type", "linkable_id"], name: "index_links_on_linkable_type_and_linkable_id"
+    end
+
+    create_table "oauth_access_grants", force: :cascade do |t|
+      t.bigint "resource_owner_id", null: false
+      t.bigint "application_id", null: false
+      t.string "token", null: false
+      t.integer "expires_in", null: false
+      t.text "redirect_uri", null: false
+      t.datetime "created_at", null: false
+      t.datetime "revoked_at"
+      t.string "scopes", default: "", null: false
+      t.index ["application_id"], name: "index_oauth_access_grants_on_application_id"
+      t.index ["resource_owner_id"], name: "index_oauth_access_grants_on_resource_owner_id"
+      t.index ["token"], name: "index_oauth_access_grants_on_token", unique: true
+    end
+
+    create_table "oauth_access_tokens", force: :cascade do |t|
+      t.bigint "resource_owner_id"
+      t.bigint "application_id", null: false
+      t.string "token", null: false
+      t.string "refresh_token"
+      t.integer "expires_in"
+      t.datetime "revoked_at"
+      t.datetime "created_at", null: false
+      t.string "scopes"
+      t.string "previous_refresh_token", default: "", null: false
+      t.index ["application_id"], name: "index_oauth_access_tokens_on_application_id"
+      t.index ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
+      t.index ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
+      t.index ["token"], name: "index_oauth_access_tokens_on_token", unique: true
+    end
+
+    create_table "oauth_applications", force: :cascade do |t|
+      t.string "name", null: false
+      t.string "uid", null: false
+      t.string "secret", null: false
+      t.text "redirect_uri", null: false
+      t.string "scopes", default: "", null: false
+      t.boolean "confidential", default: true, null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+    end
+
+    create_table "questions", force: :cascade do |t|
+      t.string "title"
+      t.text "body"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.bigint "user_id"
+      t.index ["user_id"], name: "index_questions_on_user_id"
+    end
+
+    create_table "subscriptions", force: :cascade do |t|
+      t.bigint "user_id"
+      t.bigint "question_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["question_id"], name: "index_subscriptions_on_question_id"
+      t.index ["user_id", "question_id"], name: "index_subscriptions_on_user_id_and_question_id", unique: true
+      t.index ["user_id"], name: "index_subscriptions_on_user_id"
+    end
+
+    create_table "url_files", force: :cascade do |t|
+      t.string "url", null: false
+      t.string "url_fileable_type"
+      t.bigint "url_fileable_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["url_fileable_type", "url_fileable_id"], name: "index_url_files_on_url_fileable_type_and_url_fileable_id"
+    end
+
+    create_table "users", force: :cascade do |t|
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.boolean "admin"
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
+
+    create_table "votes", force: :cascade do |t|
+      t.integer "value", default: 0
+      t.string "votable_type"
+      t.bigint "votable_id"
+      t.bigint "user_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["user_id"], name: "index_votes_on_user_id"
+      t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
+    end
+
+    add_foreign_key "achievements", "questions"
+    add_foreign_key "achievements", "users"
+    add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+    add_foreign_key "answers", "questions"
+    add_foreign_key "answers", "users"
+    add_foreign_key "authorizations", "users"
+    add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
+    add_foreign_key "oauth_access_grants", "users", column: "resource_owner_id"
+    add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
+    add_foreign_key "oauth_access_tokens", "users", column: "resource_owner_id"
+    add_foreign_key "questions", "users"
+  end
+end
